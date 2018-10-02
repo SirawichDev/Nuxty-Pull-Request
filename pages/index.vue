0c2fc9993efee5531532c9f8e48a6bcd1626a059
Chakkrit Chueangyang <30730045+Him-2C@users.noreply.github.com>
2018-10-02 10:09:07 +0700

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -8,6 +8,7 @@
       <h2 class="subtitle">
         พูลเล่นๆ
       </h2>
+      <h3> Hello Him </h3>
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
         <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
